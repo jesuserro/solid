29b0e9032d9e43993d8745f5d83bf77da835d221
olagato <olagato@gmail.com>
2023-02-13 17:52:50 +0100

    chore: commit save point

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Jesus\Solid\shape\Circle;
-use Jesus\Solid\shape\Square;
+use Jesus\Solid\shape\{Circle, Square};
 use Jesus\Solid\AreaCalculator;
 
 // Para utilizar la clase AreaCalculator simplemente instanciamos la clase y le pasamos un array con las figuras, mostrando el output al final:
