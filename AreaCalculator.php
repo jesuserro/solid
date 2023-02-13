1d31b9c38bcc8e3b17054031035e6d79d24ba337
olagato <olagato@gmail.com>
2023-02-13 13:01:51 +0100

    chore: commit save point

---
+++ AreaCalculator.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * AreaCalculator
+ * Ahora creamos la clase AreaCalculator, que recibe un array con los objetos de cada una de las figuras para ser sumadas
+ */
+class AreaCalculator
+{
+  protected $shapes;
+
+  public function __construct($shapes = array())
+  {
+      $this->shapes = $shapes;
+  }
+
+  public function sum()
+  {
+      // Aquí va la lógica para sumar todas las áreas
+  }
+
+  public function output()
+  {
+    return implode('', array(
+        "<h1>",
+            "Suma de todas las áreas: ",
+            $this->sum(),
+        "</h1>"
+      )
+    ) ;
+  }
+}
